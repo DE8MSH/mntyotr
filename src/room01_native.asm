0d32ff6cb73f45033e0f47900b640ddbf22590a4
DO7OO from 07/2021 on <de8msh@gmx.de>
2026-09-04 19:59:09 +0200

    Phase 32: add room 01 native map data

---
+++ src/room01_native.asm
@@ -0,0 +1,7 @@
+; Native room $01 generated from the exact C64 RLE stream.
+
+.data
+room01_collision_map:
+        incbin "room01-map.dat"
+room01_screen_bat:
+        incbin "room01-screen-bat.dat"
