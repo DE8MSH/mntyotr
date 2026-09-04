97f1e4add2f36f423758cfc7aecb5023d12d1273
DO7OO from 07/2021 on <de8msh@gmx.de>
2026-09-04 21:16:20 +0200

    Phase 36: include isolated Room 01 decor loader

--- src/main.asm
+++ src/main.asm
@@ -18,6 +18,7 @@
         include "monty_physics.asm"
         include "collision_banking.asm"
         include "world.asm"
+        include "room01_decor_loader.asm"
         include "room_loader.asm"
         include "monty_sprite.asm"
 
