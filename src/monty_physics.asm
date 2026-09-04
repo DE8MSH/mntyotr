75af0769df4e8bb6a86f1c0829aebd7a1dcc94ea
DO7OO from 07/2021 on <de8msh@gmx.de>
2026-09-04 16:37:52 +0200

    Port Monty tile-state and ladder movement

--- src/monty_physics.asm
+++ src/monty_physics.asm
@@ -12,6 +12,9 @@ monty_saved_left:       ds 1
 monty_saved_right:      ds 1
 monty_room:             ds 1
 monty_room_exit:        ds 1           ; 0=none, 1=left, 2=right, 3=up, 4=down
+monty_tile_state:       ds 1           ; C64: standing in/on property-3 ladder/rope tiles
+monty_is_moving:        ds 1
+monty_climbing:         ds 1
 collision_x:            ds 1
 collision_y:            ds 1
 collision_count:        ds 1
@@ -21,7 +24,6 @@ jump_delta:             ds 1
 .code
 
 monty_physics_init:
-        ; Original startGame state: Monty at ($86,$b0), facing left.
         lda     #$86
         sta     <monty_x
         lda     #$b0
@@ -35,6 +37,9 @@ monty_physics_init:
         stz     <monty_saved_right
         stz     <monty_room
         stz     <monty_room_exit
+        stz     <monty_tile_state
+        stz     <monty_is_moving
+        stz     <monty_climbing
         rts
 
 room00_get_tile_property:
@@ -73,16 +78,14 @@ room00_get_property_xy:
         call    room00_get_tile_xy
         jmp     room00_get_tile_property
 
-monty_check_tile_right:
-        lda     <monty_x
-        sec
-        sbc     #$0c
-        and     #$03
-        bne     .clear
+; Scan Monty's 2x3 logical footprint for property 3, mirroring the C64
+; UpdateTileFlags state transition. This state enables UP/DOWN ladder/rope
+; movement and selects the authentic climb animation.
+monty_update_tile_state:
+        stz     <monty_tile_state
         lda     <monty_y
         sec
         sbc     #$32
-        pha
         lsr     a
         lsr     a
         lsr     a
@@ -92,326 +95,397 @@ monty_check_tile_right:
         sbc     #$0c
         lsr     a
         lsr     a
-        clc
-        adc     #$02
         tax
-        pla
-        and     #$07
-        bne     .three
-        lda     #2
-        bra     .count
-.three:
         lda     #3
-.count:
         sta     <collision_count
-.loop:
+.row:
         phx
         phy
         call    room00_get_property_xy
         ply
         plx
-        cmp     #$01
-        beq     .solid
+        cmp     #3
+        beq     .surface
+        inx
+        phx
+        phy
+        call    room00_get_property_xy
+        ply
+        plx
+        cmp     #3
+        beq     .surface
+        dex
         iny
         dec     <collision_count
-        bne     .loop
-.clear:
-        clc
+        bne     .row
         rts
-.solid:
+.surface:
+        lda     #1
+        sta     <monty_tile_state
+        rts
+
+monty_check_tile_right:
+        lda <monty_x
         sec
+        sbc #$0c
+        and #$03
+        bne .clear
+        lda <monty_y
+        sec
+        sbc #$32
+        pha
+        lsr a
+        lsr a
+        lsr a
+        tay
+        lda <monty_x
+        sec
+        sbc #$0c
+        lsr a
+        lsr a
+        clc
+        adc #$02
+        tax
+        pla
+        and #$07
+        bne .three
+        lda #2
+        bra .count
+.three: lda #3
+.count: sta <collision_count
+.loop:
+        phx
+        phy
+        call room00_get_property_xy
+        ply
+        plx
+        cmp #$01
+        beq .solid
+        iny
+        dec <collision_count
+        bne .loop
+.clear: clc
+        rts
+.solid: sec
         rts
 
 monty_check_tile_left:
-        lda     <monty_x
+        lda <monty_x
         sec
-        sbc     #$0c
-        and     #$03
-        bne     .clear
-        lda     <monty_y
+        sbc #$0c
+        and #$03
+        bne .clear
+        lda <monty_y
         sec
-        sbc     #$32
+        sbc #$32
         pha
-        lsr     a
-        lsr     a
-        lsr     a
+        lsr a
+        lsr a
+        lsr a
         tay
-        lda     <monty_x
+        lda <monty_x
         sec
-        sbc     #$0c
-        lsr     a
-        lsr     a
+        sbc #$0c
+        lsr a
+        lsr a
         sec
-        sbc     #$01
+        sbc #$01
         tax
         pla
-        and     #$07
-        bne     .three
-        lda     #2
-        bra     .count
-.three:
-        lda     #3
-.count:
-        sta     <collision_count
+        and #$07
+        bne .three
+        lda #2
+        bra .count
+.three: lda #3
+.count: sta <collision_count
 .loop:
         phx
         phy
-        call    room00_get_property_xy
+        call room00_get_property_xy
         ply
         plx
-        cmp     #$01
-        beq     .solid
+        cmp #$01
+        beq .solid
         iny
-        dec     <collision_count
-        bne     .loop
-.clear:
-        clc
+        dec <collision_count
+        bne .loop
+.clear: clc
         rts
-.solid:
-        sec
+.solid: sec
         rts
 
 monty_check_tile_above:
-        lda     <monty_y
+        lda <monty_y
         sec
-        sbc     #$32
-        and     #$07
-        bne     .clear
-        lda     <monty_y
+        sbc #$32
+        and #$07
+        bne .clear
+        lda <monty_y
         sec
-        sbc     #$32
-        lsr     a
-        lsr     a
-        lsr     a
+        sbc #$32
+        lsr a
+        lsr a
+        lsr a
         sec
-        sbc     #$01
+        sbc #$01
         tay
-        lda     <monty_x
+        lda <monty_x
         sec
-        sbc     #$0c
+        sbc #$0c
         pha
-        lsr     a
-        lsr     a
+        lsr a
+        lsr a
         tax
         pla
-        and     #$03
-        beq     .two
-        lda     #3
-        bra     .count
-.two:
-        lda     #2
-.count:
-        sta     <collision_count
+        and #$03
+        beq .two
+        lda #3
+        bra .count
+.two: lda #2
+.count: sta <collision_count
 .loop:
         phx
         phy
-        call    room00_get_property_xy
+        call room00_get_property_xy
         ply
         plx
-        cmp     #$01
-        beq     .solid
+        cmp #$01
+        beq .solid
         inx
-        dec     <collision_count
-        bne     .loop
-.clear:
-        clc
+        dec <collision_count
+        bne .loop
+.clear: clc
         rts
-.solid:
-        sec
+.solid: sec
         rts
 
 monty_check_tile_below:
-        lda     <monty_y
+        lda <monty_y
         sec
-        sbc     #$32
-        and     #$07
-        bne     .clear
-        lda     <monty_y
+        sbc #$32
+        and #$07
+        bne .clear
+        lda <monty_y
         sec
-        sbc     #$32
-        lsr     a
-        lsr     a
-        lsr     a
+        sbc #$32
+        lsr a
+        lsr a
+        lsr a
         clc
-        adc     #$02
+        adc #$02
         tay
-        lda     <monty_x
+        lda <monty_x
         sec
-        sbc     #$0c
+        sbc #$0c
         pha
-        lsr     a
-        lsr     a
+        lsr a
+        lsr a
         tax
         pla
-        and     #$03
-        beq     .two
-        lda     #3
-        bra     .count
-.two:
-        lda     #2
-.count:
-        sta     <collision_count
+        and #$03
+        beq .two
+        lda #3
+        bra .count
+.two: lda #2
+.count: sta <collision_count
 .loop:
         phx
         phy
-        call    room00_get_property_xy
+        call room00_get_property_xy
         ply
         plx
-        cmp     #$01
-        beq     .solid
-        cmp     #$02
-        beq     .solid
-        cmp     #$03
-        beq     .solid
+        cmp #$01
+        beq .solid
+        cmp #$02
+        beq .solid
+        cmp #$03
+        beq .solid
         inx
-        dec     <collision_count
-        bne     .loop
-.clear:
-        clc
+        dec <collision_count
+        bne .loop
+.clear: clc
         rts
-.solid:
-        sec
+.solid: sec
         rts
 
 monty_toggle_step_gate:
-        inc     <monty_step_phase
-        lda     <monty_step_phase
-        and     #$01
+        inc <monty_step_phase
+        lda <monty_step_phase
+        and #$01
         rts
 
 monty_jump_start:
-        lda     <monty_jump_phase
-        bne     .done
-        lda     #1
-        sta     <monty_jump_phase
-        stz     <monty_jump_index
-        lda     joynow
-        and     #$80
-        sta     <monty_saved_left
-        lda     joynow
-        and     #$20
-        sta     <monty_saved_right
-.done:
-        rts
+        lda <monty_jump_phase
+        bne .done
+        lda #1
+        sta <monty_jump_phase
+        stz <monty_jump_index
+        lda joynow
+        and #$80
+        sta <monty_saved_left
+        lda joynow
+        and #$20
+        sta <monty_saved_right
+.done:  rts
 
 monty_check_room_edges:
-        stz     <monty_room_exit
-        lda     <monty_x
-        cmp     #$15
-        bcc     .left_exit
-        cmp     #$9c
-        bcs     .right_exit
-        lda     <monty_y
-        cmp     #$4c
-        bcc     .up_exit
+        stz <monty_room_exit
+        lda <monty_x
+        cmp #$15
+        bcc .left_exit
+        cmp #$9c
+        bcs .right_exit
+        lda <monty_y
+        cmp #$4c
+        bcc .up_exit
         rts
 .left_exit:
-        lda     #1
-        sta     <monty_room_exit
-        lda     #$9b
-        sta     <monty_x
+        lda #1
+        sta <monty_room_exit
+        lda #$9b
+        sta <monty_x
         rts
 .right_exit:
-        lda     #2
-        sta     <monty_room_exit
-        lda     #$15
-        sta     <monty_x
+        lda #2
+        sta <monty_room_exit
+        lda #$15
+        sta <monty_x
         rts
 .up_exit:
-        lda     #3
-        sta     <monty_room_exit
-        lda     #$da
-        sta     <monty_y
+        lda #3
+        sta <monty_room_exit
+        lda #$da
+        sta <monty_y
         rts
 
+; PCE bits after HuC joypad transform: I=$01 UP=$10 RIGHT=$20 DOWN=$40 LEFT=$80.
 monty_update_input:
-        lda     joynow
-        and     #$01
-        beq     .horizontal
-        lda     <monty_jump_phase
-        bne     .horizontal
-        call    monty_jump_start
+        stz <monty_is_moving
+        stz <monty_climbing
+        call monty_update_tile_state
+        lda joynow
+        and #$01
+        beq .directions
+        lda <monty_jump_phase
+        bne .directions
+        lda <monty_tile_state
+        bne .directions
+        call monty_jump_start
+.directions:
+        ; C64 only accepts vertical player input while tile_state is active and
+        ; no jump action is in progress.
+        lda <monty_jump_phase
+        bne .horizontal
+        lda <monty_tile_state
+        beq .horizontal
+        lda joynow
+        and #$10
+        bne .up
+        lda joynow
+        and #$40
+        bne .down
 .horizontal:
-        lda     <monty_jump_phase
-        beq     .live_pad
-        lda     <monty_saved_left
-        bne     .left
-        lda     <monty_saved_right
-        bne     .right
+        lda <monty_jump_phase
+        beq .live_pad
+        lda <monty_saved_left
+        bne .left
+        lda <monty_saved_right
+        bne .right
         rts
 .live_pad:
-        lda     joynow
-        and     #$80
-        bne     .left
-        lda     joynow
-        and     #$20
-        bne     .right
+        lda joynow
+        and #$80
+        bne .left
+        lda joynow
+        and #$20
+        bne .right
+        rts
+.up:
+        call monty_check_tile_above
+        bcs .done_vertical
+        dec <monty_y
+        lda #1
+        sta <monty_is_moving
+        sta <monty_climbing
+        call monty_check_room_edges
+.done_vertical:
+        rts
+.down:
+        call monty_check_tile_below
+        bcs .done_down
+        inc <monty_y
+        lda #1
+        sta <monty_is_moving
+        sta <monty_climbing
+.done_down:
         rts
 .left:
-        call    monty_check_tile_left
-        bcs     .done
-        lda     #$80
-        sta     <monty_facing
-        call    monty_toggle_step_gate
-        beq     .done
-        dec     <monty_x
-        call    monty_check_room_edges
-.done:
-        rts
+        call monty_check_tile_left
+        bcs .done
+        lda #$80
+        sta <monty_facing
+        call monty_toggle_step_gate
+        beq .done
+        dec <monty_x
+        lda #1
+        sta <monty_is_moving
+        call monty_check_room_edges
+.done:  rts
 .right:
-        call    monty_check_tile_right
-        bcs     .done_right
-        stz     <monty_facing
-        call    monty_toggle_step_gate
-        bne     .done_right
-        inc     <monty_x
-        call    monty_check_room_edges
+        call monty_check_tile_right
+        bcs .done_right
+        stz <monty_facing
+        call monty_toggle_step_gate
+        bne .done_right
+        inc <monty_x
+        lda #1
+        sta <monty_is_moving
+        call monty_check_room_edges
 .done_right:
         rts
 
 monty_jump_step:
-        lda     <monty_jump_phase
-        beq     .done
-        cmp     #1
-        bne     .descent
-        call    monty_check_tile_above
-        bcs     .switch_down
-        ldx     <monty_jump_index
-        lda     monty_jump_arc_up,x
-        cmp     #$ff
-        beq     .switch_down
-        sta     <jump_delta
-        lda     <monty_y
+        lda <monty_jump_phase
+        beq .done
+        cmp #1
+        bne .descent
+        call monty_check_tile_above
+        bcs .switch_down
+        ldx <monty_jump_index
+        lda monty_jump_arc_up,x
+        cmp #$ff
+        beq .switch_down
+        sta <jump_delta
+        lda <monty_y
         sec
-        sbc     <jump_delta
-        sta     <monty_y
-        inc     <monty_jump_index
-        call    monty_check_room_edges
+        sbc <jump_delta
+        sta <monty_y
+        inc <monty_jump_index
+        call monty_check_room_edges
         rts
 .switch_down:
-        lda     #2
-        sta     <monty_jump_phase
-        stz     <monty_jump_index
+        lda #2
+        sta <monty_jump_phase
+        stz <monty_jump_index
         rts
 .descent:
-        call    monty_check_tile_below
-        bcs     .land
-        ldx     <monty_jump_index
-        lda     monty_jump_arc_down,x
-        cmp     #$ff
-        beq     .land
+        call monty_check_tile_below
+        bcs .land
+        ldx <monty_jump_index
+        lda monty_jump_arc_down,x
+        cmp #$ff
+        beq .land
         clc
-        adc     <monty_y
-        sta     <monty_y
-        inc     <monty_jump_index
+        adc <monty_y
+        sta <monty_y
+        inc <monty_jump_index
         rts
 .land:
-        stz     <monty_jump_phase
-        stz     <monty_jump_index
-        stz     <monty_saved_left
-        stz     <monty_saved_right
-.done:
-        rts
+        stz <monty_jump_phase
+        stz <monty_jump_index
+        stz <monty_saved_left
+        stz <monty_saved_right
+.done:  rts
 
 .data
 room00_tile_properties:
