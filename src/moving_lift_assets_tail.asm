5f79ef882bf1f5748a93d96cc8480fea67452d25
DO7OO from 07/2021 on <de8msh@gmx.de>
2026-09-05 15:50:01 +0200

    Align lift sprite colours with unified C64 palette

--- src/moving_lift_assets_tail.asm
+++ src/moving_lift_assets_tail.asm
@@ -6,7 +6,8 @@ moving_lift_patterns:
         incbin "lift-sprites.dat"
 
 ; VIC multicolour mapping used by lift_sprite.py:
-; 0 transparent, 1 shared red, 2 sprite-specific pulsing grey (fixed light grey
-; for this first PCE pass), 3 shared light-red.
+; 0 transparent, 1 shared C64 red $02, 2 sprite-specific pulsing grey (fixed
+; light grey $0f for this pass), 3 shared C64 light-red $0a.
+; Values use the same GGGRRRBBB C64 quantization as the room palettes.
 moving_lift_palette:
-        dw $000,$099,$16d,$0bb,$000,$000,$000,$000,$000,$000,$000,$000,$000,$000,$000,$000
+        dw $000,$062,$16d,$0eb,$000,$000,$000,$000,$000,$000,$000,$000,$000,$000,$000,$000
