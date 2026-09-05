07915ee233072363b1691ea561421b5466a9d682
DO7OO from 07/2021 on <de8msh@gmx.de>
2026-09-05 15:14:34 +0200

    Add piledriver sprite assets

---
+++ src/standard_piledriver_assets_tail.asm
@@ -0,0 +1,8 @@
+piledriver_patterns:
+        incbin "piledriver-sprites.dat"
+
+; Sprite palette 19 / low nibble 3. Original shaft uses light/mid/dark greys;
+; the authentic monochrome head silhouette uses index 1 here.
+piledriver_sprite_palette:
+        dw $000,$1b6,$000,$000,$000,$000,$000,$000
+        dw $000,$000,$000,$000,$000,$000,$000,$000
