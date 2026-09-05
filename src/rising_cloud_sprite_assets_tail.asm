1ca3df747bbee35f365af3e16760c0d4ab625831
DO7OO from 07/2021 on <de8msh@gmx.de>
2026-09-05 14:09:42 +0200

    Add rising cloud sprite assets

---
+++ src/rising_cloud_sprite_assets_tail.asm
@@ -0,0 +1,9 @@
+; Authentic Room-$01 rising cloud frames $98/$99/$9A converted to PCE sprites.
+
+.data
+rising_cloud_patterns:
+        incbin "cloud-sprites.dat"
+
+; Original cloud colour is C64 white ($01). Sprite palette slot 18 -> index 2.
+rising_cloud_sprite_palette:
+        dw $000,$1ff,$000,$000,$000,$000,$000,$000,$000,$000,$000,$000,$000,$000,$000,$000
