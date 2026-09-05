2acd5a54039b7dacf61f564703d59ced3ce1d860
DO7OO from 07/2021 on <de8msh@gmx.de>
2026-09-05 21:12:01 +0200

    Make tail-room loader table driven

--- src/room050c_loader.asm
+++ src/room050c_loader.asm
@@ -1,117 +1,85 @@
-; Loader extension for newer tail rooms $05/$09/$0C.
-; Keep the already-confirmed core room_loader.asm stable: this small dispatcher
-; handles added rooms first, then falls through to the proven older loader.
+; Compact table-driven loader for tail rooms $05-$09 plus $0C.
+; Rooms $06-$08 complete the upper house/Pie Are Square route. Far-pointer
+; tables stay in code space so selecting an asset never depends on the ROM bank
+; currently mapped into MPR3/MPR4. Bulk assets remain in the ROM tail.
+
+.zp
+room_ext_index: ds 1
 
 .code
 
 room_load_pending_extended:
         lda     <world_pending_room
         cmp     #$05
-        beq     .room05
-        cmp     #$09
-        beq     .room09
+        bcc     .core_loader
+        cmp     #$0a
+        bcc     .range_05_09
         cmp     #$0c
-        beq     .room0c
+        bne     .core_loader
+        lda     #5
+        bra     .have_index
+.range_05_09:
+        sec
+        sbc     #$05
+.have_index:
+        sta     <room_ext_index
+
+        ; Upload this room's nine custom character patterns.
+        ldx     <room_ext_index
+        lda     room_ext_patterns_lo,x
+        sta     <_bp
+        lda     room_ext_patterns_hi,x
+        sta     <_bp+1
+        ldy     room_ext_patterns_bank,x
+        call    room_upload_9_patterns
+
+        ; Draw its exact 36x20 C64 playfield window.
+        ldx     <room_ext_index
+        lda     room_ext_bat_lo,x
+        sta     <_bp
+        lda     room_ext_bat_hi,x
+        sta     <_bp+1
+        ldy     room_ext_bat_bank,x
+        call    room_draw_native_36x20
+
+        ; Cache 640 collision cells + 8 tile properties in the shared RAM area.
+        ldx     <room_ext_index
+        lda     room_ext_collision_lo,x
+        sta     <_bp
+        lda     room_ext_collision_hi,x
+        sta     <_bp+1
+        ldy     room_ext_collision_bank,x
+        call    room_tail_cache_collision
+
+        lda     <world_pending_room
+        sta     <monty_room
+        stz     <world_transition_ready
+        sec
+        rts
+
+.core_loader:
         jmp     room_load_pending
 
-.room05:
-        call    room05_upload_patterns
-        call    room05_draw_native
-        call    room05_cache_collision
-        lda     #$05
-        sta     <monty_room
-        stz     <world_transition_ready
-        sec
-        rts
+; index 0..5 = rooms $05,$06,$07,$08,$09,$0C.
+; These descriptors replace dozens of room-specific wrapper instructions,
+; leaving Bank 0 headroom while the world continues to grow.
+room_ext_patterns_lo:
+        db <room05_patterns,<room06_patterns,<room07_patterns,<room08_patterns,<room09_patterns,<room0c_patterns
+room_ext_patterns_hi:
+        db >room05_patterns,>room06_patterns,>room07_patterns,>room08_patterns,>room09_patterns,>room0c_patterns
+room_ext_patterns_bank:
+        db BANK(room05_patterns),BANK(room06_patterns),BANK(room07_patterns),BANK(room08_patterns),BANK(room09_patterns),BANK(room0c_patterns)
 
-.room09:
-        call    room09_upload_patterns
-        call    room09_draw_native
-        call    room09_cache_collision
-        lda     #$09
-        sta     <monty_room
-        stz     <world_transition_ready
-        sec
-        rts
+room_ext_bat_lo:
+        db <room05_screen_bat,<room06_screen_bat,<room07_screen_bat,<room08_screen_bat,<room09_screen_bat,<room0c_screen_bat
+room_ext_bat_hi:
+        db >room05_screen_bat,>room06_screen_bat,>room07_screen_bat,>room08_screen_bat,>room09_screen_bat,>room0c_screen_bat
+room_ext_bat_bank:
+        db BANK(room05_screen_bat),BANK(room06_screen_bat),BANK(room07_screen_bat),BANK(room08_screen_bat),BANK(room09_screen_bat),BANK(room0c_screen_bat)
 
-.room0c:
-        call    room0c_upload_patterns
-        call    room0c_draw_native
-        call    room0c_cache_collision
-        lda     #$0c
-        sta     <monty_room
-        stz     <world_transition_ready
-        sec
-        rts
-
-room05_cache_collision:
-        lda     #<room05_collision_map_rom
-        sta     <_bp
-        lda     #>room05_collision_map_rom
-        sta     <_bp+1
-        ldy     #BANK(room05_collision_map_rom)
-        jmp     room_tail_cache_collision
-
-room09_cache_collision:
-        lda     #<room09_collision_map_rom
-        sta     <_bp
-        lda     #>room09_collision_map_rom
-        sta     <_bp+1
-        ldy     #BANK(room09_collision_map_rom)
-        jmp     room_tail_cache_collision
-
-room0c_cache_collision:
-        lda     #<room0c_collision_map_rom
-        sta     <_bp
-        lda     #>room0c_collision_map_rom
-        sta     <_bp+1
-        ldy     #BANK(room0c_collision_map_rom)
-        jmp     room_tail_cache_collision
-
-room05_upload_patterns:
-        lda     #<room05_patterns
-        sta     <_bp
-        lda     #>room05_patterns
-        sta     <_bp+1
-        ldy     #BANK(room05_patterns)
-        jmp     room_upload_9_patterns
-
-room09_upload_patterns:
-        lda     #<room09_patterns
-        sta     <_bp
-        lda     #>room09_patterns
-        sta     <_bp+1
-        ldy     #BANK(room09_patterns)
-        jmp     room_upload_9_patterns
-
-room0c_upload_patterns:
-        lda     #<room0c_patterns
-        sta     <_bp
-        lda     #>room0c_patterns
-        sta     <_bp+1
-        ldy     #BANK(room0c_patterns)
-        jmp     room_upload_9_patterns
-
-room05_draw_native:
-        lda     #<room05_screen_bat
-        sta     <_bp
-        lda     #>room05_screen_bat
-        sta     <_bp+1
-        ldy     #BANK(room05_screen_bat)
-        jmp     room_draw_native_36x20
-
-room09_draw_native:
-        lda     #<room09_screen_bat
-        sta     <_bp
-        lda     #>room09_screen_bat
-        sta     <_bp+1
-        ldy     #BANK(room09_screen_bat)
-        jmp     room_draw_native_36x20
-
-room0c_draw_native:
-        lda     #<room0c_screen_bat
-        sta     <_bp
-        lda     #>room0c_screen_bat
-        sta     <_bp+1
-        ldy     #BANK(room0c_screen_bat)
-        jmp     room_draw_native_36x20
+room_ext_collision_lo:
+        db <room05_collision_map_rom,<room06_collision_map_rom,<room07_collision_map_rom,<room08_collision_map_rom,<room09_collision_map_rom,<room0c_collision_map_rom
+room_ext_collision_hi:
+        db >room05_collision_map_rom,>room06_collision_map_rom,>room07_collision_map_rom,>room08_collision_map_rom,>room09_collision_map_rom,>room0c_collision_map_rom
+room_ext_collision_bank:
+        db BANK(room05_collision_map_rom),BANK(room06_collision_map_rom),BANK(room07_collision_map_rom),BANK(room08_collision_map_rom),BANK(room09_collision_map_rom),BANK(room0c_collision_map_rom)
