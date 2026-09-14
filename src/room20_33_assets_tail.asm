e75db44511fa4f338f240ff6dc0704602894ef71
DO7OO from 07/2021 on <de8msh@gmx.de>
2026-09-14 11:55:55 +0200

    Add Room20-33 banked asset tail

---
+++ src/room20_33_assets_tail.asm
@@ -0,0 +1,183 @@
+; Generated ROM-tail assets for exact C64 Rooms $20-$33.
+; Each collision map is immediately followed by its 8-byte property table.
+
+.data
+room20_patterns:
+        incbin "room20-patterns.dat"
+room20_collision_map_rom:
+        incbin "room20-map.dat"
+room20_tile_properties_rom:
+        incbin "room20-properties.dat"
+room20_screen_bat:
+        incbin "room20-screen-bat.dat"
+
+room21_patterns:
+        incbin "room21-patterns.dat"
+room21_collision_map_rom:
+        incbin "room21-map.dat"
+room21_tile_properties_rom:
+        incbin "room21-properties.dat"
+room21_screen_bat:
+        incbin "room21-screen-bat.dat"
+
+room22_patterns:
+        incbin "room22-patterns.dat"
+room22_collision_map_rom:
+        incbin "room22-map.dat"
+room22_tile_properties_rom:
+        incbin "room22-properties.dat"
+room22_screen_bat:
+        incbin "room22-screen-bat.dat"
+
+room23_patterns:
+        incbin "room23-patterns.dat"
+room23_collision_map_rom:
+        incbin "room23-map.dat"
+room23_tile_properties_rom:
+        incbin "room23-properties.dat"
+room23_screen_bat:
+        incbin "room23-screen-bat.dat"
+
+room24_patterns:
+        incbin "room24-patterns.dat"
+room24_collision_map_rom:
+        incbin "room24-map.dat"
+room24_tile_properties_rom:
+        incbin "room24-properties.dat"
+room24_screen_bat:
+        incbin "room24-screen-bat.dat"
+
+room25_patterns:
+        incbin "room25-patterns.dat"
+room25_collision_map_rom:
+        incbin "room25-map.dat"
+room25_tile_properties_rom:
+        incbin "room25-properties.dat"
+room25_screen_bat:
+        incbin "room25-screen-bat.dat"
+
+room26_patterns:
+        incbin "room26-patterns.dat"
+room26_collision_map_rom:
+        incbin "room26-map.dat"
+room26_tile_properties_rom:
+        incbin "room26-properties.dat"
+room26_screen_bat:
+        incbin "room26-screen-bat.dat"
+
+room27_patterns:
+        incbin "room27-patterns.dat"
+room27_collision_map_rom:
+        incbin "room27-map.dat"
+room27_tile_properties_rom:
+        incbin "room27-properties.dat"
+room27_screen_bat:
+        incbin "room27-screen-bat.dat"
+
+room28_patterns:
+        incbin "room28-patterns.dat"
+room28_collision_map_rom:
+        incbin "room28-map.dat"
+room28_tile_properties_rom:
+        incbin "room28-properties.dat"
+room28_screen_bat:
+        incbin "room28-screen-bat.dat"
+
+room29_patterns:
+        incbin "room29-patterns.dat"
+room29_collision_map_rom:
+        incbin "room29-map.dat"
+room29_tile_properties_rom:
+        incbin "room29-properties.dat"
+room29_screen_bat:
+        incbin "room29-screen-bat.dat"
+
+room2a_patterns:
+        incbin "room2a-patterns.dat"
+room2a_collision_map_rom:
+        incbin "room2a-map.dat"
+room2a_tile_properties_rom:
+        incbin "room2a-properties.dat"
+room2a_screen_bat:
+        incbin "room2a-screen-bat.dat"
+
+room2b_patterns:
+        incbin "room2b-patterns.dat"
+room2b_collision_map_rom:
+        incbin "room2b-map.dat"
+room2b_tile_properties_rom:
+        incbin "room2b-properties.dat"
+room2b_screen_bat:
+        incbin "room2b-screen-bat.dat"
+
+room2c_patterns:
+        incbin "room2c-patterns.dat"
+room2c_collision_map_rom:
+        incbin "room2c-map.dat"
+room2c_tile_properties_rom:
+        incbin "room2c-properties.dat"
+room2c_screen_bat:
+        incbin "room2c-screen-bat.dat"
+
+room2d_patterns:
+        incbin "room2d-patterns.dat"
+room2d_collision_map_rom:
+        incbin "room2d-map.dat"
+room2d_tile_properties_rom:
+        incbin "room2d-properties.dat"
+room2d_screen_bat:
+        incbin "room2d-screen-bat.dat"
+
+room2e_patterns:
+        incbin "room2e-patterns.dat"
+room2e_collision_map_rom:
+        incbin "room2e-map.dat"
+room2e_tile_properties_rom:
+        incbin "room2e-properties.dat"
+room2e_screen_bat:
+        incbin "room2e-screen-bat.dat"
+
+room2f_patterns:
+        incbin "room2f-patterns.dat"
+room2f_collision_map_rom:
+        incbin "room2f-map.dat"
+room2f_tile_properties_rom:
+        incbin "room2f-properties.dat"
+room2f_screen_bat:
+        incbin "room2f-screen-bat.dat"
+
+room30_patterns:
+        incbin "room30-patterns.dat"
+room30_collision_map_rom:
+        incbin "room30-map.dat"
+room30_tile_properties_rom:
+        incbin "room30-properties.dat"
+room30_screen_bat:
+        incbin "room30-screen-bat.dat"
+
+room31_patterns:
+        incbin "room31-patterns.dat"
+room31_collision_map_rom:
+        incbin "room31-map.dat"
+room31_tile_properties_rom:
+        incbin "room31-properties.dat"
+room31_screen_bat:
+        incbin "room31-screen-bat.dat"
+
+room32_patterns:
+        incbin "room32-patterns.dat"
+room32_collision_map_rom:
+        incbin "room32-map.dat"
+room32_tile_properties_rom:
+        incbin "room32-properties.dat"
+room32_screen_bat:
+        incbin "room32-screen-bat.dat"
+
+room33_patterns:
+        incbin "room33-patterns.dat"
+room33_collision_map_rom:
+        incbin "room33-map.dat"
+room33_tile_properties_rom:
+        incbin "room33-properties.dat"
+room33_screen_bat:
+        incbin "room33-screen-bat.dat"
