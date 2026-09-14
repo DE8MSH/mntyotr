07b9b0146e6df171581774cce66f536c2e46af63
DO7OO from 07/2021 on <de8msh@gmx.de>
2026-09-14 08:39:48 +0200

    Add authentic Room10-1F gems

--- src/gem_assets_tail.asm
+++ src/gem_assets_tail.asm
@@ -1,4 +1,4 @@
-; Banked collectible data for rooms $00-$0F.
+; Banked collectible data for rooms $00-$1F.
 ; Records are (room, target_x, target_y, BAT_lo, BAT_hi) and come from the
 ; original C64 FreedomKit.Data.item_tbl positions.
 
@@ -24,6 +24,36 @@ gem_records:
         db $0e,$7d,$ac,$de,$03
         db $0f,$1d,$6c,$c6,$01
         db $0f,$69,$6c,$d9,$01
+        db $11,$71,$6c,$db,$01
+        db $11,$71,$bc,$5b,$04
+        db $12,$85,$8c,$e0,$02
+        db $12,$71,$c4,$9b,$04
+        db $12,$4d,$84,$92,$02
+        db $13,$61,$ac,$d7,$03
+        db $13,$45,$d4,$10,$05
+        db $14,$75,$8c,$dc,$02
+        db $15,$41,$6c,$cf,$01
+        db $15,$61,$8c,$d7,$02
+        db $15,$4d,$a4,$92,$03
+        db $16,$41,$94,$0f,$03
+        db $16,$7d,$a4,$9e,$03
+        db $17,$65,$cc,$d8,$04
+        db $18,$21,$54,$07,$01
+        db $18,$21,$cc,$c7,$04
+        db $19,$35,$d4,$0c,$05
+        db $1a,$59,$94,$15,$03
+        db $1b,$21,$8c,$c7,$02
+        db $1b,$1d,$bc,$46,$04
+        db $1b,$71,$74,$1b,$02
+        db $1c,$41,$bc,$4f,$04
+        db $1d,$79,$6c,$dd,$01
+        db $1d,$65,$94,$18,$03
+        db $1d,$21,$a4,$87,$03
+        db $1e,$89,$9c,$61,$03
+        db $1e,$85,$64,$a0,$01
+        db $1e,$49,$9c,$51,$03
+        db $1f,$3d,$cc,$ce,$04
+        db $1f,$51,$bc,$53,$04
 
 ; Original C64 font character $34 used by RoomEntitiesInit for room collectibles:
 ;   3c 42 df c7 fb fb 46 3c
