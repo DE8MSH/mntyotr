e1ce68f0a131b15a5a1b81fe70d92bae552306b0
DO7OO from 07/2021 on <de8msh@gmx.de>
2026-09-14 15:53:42 +0200

    Use long-safe enemy record sentinel branch

--- src/enemy_room10_1f_runtime.asm
+++ src/enemy_room10_1f_runtime.asm
@@ -115,7 +115,9 @@ enemy_room10_1f_last_room: ds 1
 .decode_next:
         lda [_bp],y
         cmp #$ff
-        beq .decode_done
+        bne .have_record
+        jmp .decode_done
+.have_record:
         sty enemy_tmp_record_y
         tax
         lda .colour_tbl,x
